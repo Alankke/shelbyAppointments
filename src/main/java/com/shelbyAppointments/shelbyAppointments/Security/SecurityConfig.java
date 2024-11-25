cc40bc35fc82e12b33a9f579bc2d14c45b79be75
alankke <alaneduardokennedy@gmail.com>
2024-11-25 19:12:41 -0300

    errors

--- src/main/java/com/shelbyAppointments/shelbyAppointments/Security/SecurityConfig.java
+++ src/main/java/com/shelbyAppointments/shelbyAppointments/Security/SecurityConfig.java
@@ -53,8 +53,7 @@ public class SecurityConfig {
    public CorsConfigurationSource corsConfigurationSource() {
        CorsConfiguration configuration = new CorsConfiguration();
        configuration.setAllowedOrigins(Arrays.asList(
-           "http://localhost:5173",
-           "https://shelby-frontend-jade.vercel.app"
+           "https://shelbyturnos.vercel.app"
        ));
        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
        configuration.setAllowedHeaders(Arrays.asList("*"));
