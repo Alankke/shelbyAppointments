43d449eacc16835a703dec2a19f83813c25aeb52
alankke <alaneduardokennedy@gmail.com>
2024-11-25 19:44:12 -0300

    remove services

--- src/main/java/com/shelbyAppointments/shelbyAppointments/Service/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shelbyAppointments.shelbyAppointments.Service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service, String> {
-}
